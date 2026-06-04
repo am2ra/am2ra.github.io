4f721b95301452eeab11ba044b5e74e3d506b75a
amara <155475152+am2ra@users.noreply.github.com>
2026-06-04 12:57:07 +0100

    Woah

--- java.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
-btn.addEventListener('click', () => {
-    navigator.clipboard.writeText(input.value);
-}
-
-)
